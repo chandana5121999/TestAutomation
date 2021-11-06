44fb34000bddafef968c2c9f91d9a0aa753d14e8
chandana5121999 <chandana57554@gmail.com>
2021-11-06 17:05:58 +0530

    EvenNumbers

---
+++ Web-Automation/src/com/sgtesting/testscript/EvenNumbers.java
@@ -0,0 +1,18 @@
+package com.sgtesting.testscript;
+
+public class EvenNumbers {
+
+	public static void main(String[] args)
+	{
+		for(int i=10;i<=20;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("Even Numbers" +i);
+			}
+		}
+		
+
+	}
+
+}
